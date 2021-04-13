0566d00b1f556b7291736e251c4ac595b356c997
thinhle99 <thinhlx99@gmail.com>
2021-04-13 22:51:09 +0700

    information of drinks

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -73,7 +73,10 @@ export default function SingleCocktail() {
     } = cocktail
     return (
       <section className='section cocktail-section'>
-
+        <Link to='/' className='btn btn-primary'>
+          back home
+        </Link>
+        <h2 className='section-title'>{name}</h2>
         <div className='drink'>
           <img src={image} alt={name}></img>
           <div className='drink-info'>

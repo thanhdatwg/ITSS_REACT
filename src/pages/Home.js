99add7dfacf19f6900641302a3ed1763730d5476
unknown <anhviet9819@gmail.com>
2021-04-13 22:34:24 +0700

    add searchForm

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
 export default function Home() {
   return (
     <main>
+      <SearchForm />
       <CocktailList />
     </main>
   );
